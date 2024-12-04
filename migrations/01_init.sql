16bd253e575c4867434831a086150d30781c20c6
Aleksey Fomichev <alfom921@yandex.ru>
2024-12-04 16:39:49 +0300

    db refact

--- migrations/01_init.sql
+++ migrations/01_init.sql
@@ -8,41 +8,15 @@ DO $$
             created_on TIMESTAMP NOT NULL default CURRENT_TIMESTAMP
         );
 
-        CREATE TABLE IF NOT EXISTS public.pairs_data(
-            id serial PRIMARY KEY,
-            account_id integer not null,
-            key VARCHAR (255) NOT NULL,
-            pwd VARCHAR (255) NOT NULL,
-            meta VARCHAR (255),
-            uploaded_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
-            FOREIGN KEY (account_id) REFERENCES public.account (id) ON DELETE CASCADE
-        );
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'datatype') THEN
+            CREATE TYPE datatype AS ENUM('PAIR','TEXT', 'BINARY', 'CARD');
+        END IF;
 
-        CREATE TABLE IF NOT EXISTS public.simple_data(
+        CREATE TABLE IF NOT EXISTS public.data(
             id serial PRIMARY KEY,
             account_id integer not null,
-            text_data TEXT NOT NULL,
-            meta VARCHAR (255),
-            uploaded_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
-            FOREIGN KEY (account_id) REFERENCES public.account (id) ON DELETE CASCADE
-        );
-
-        CREATE TABLE IF NOT EXISTS public.binary_data(
-            id serial PRIMARY KEY,
-            account_id integer not null,
-            binary_data bytea NOT NULL,
-            meta VARCHAR (255),
-            uploaded_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
-            FOREIGN KEY (account_id) REFERENCES public.account (id) ON DELETE CASCADE
-        );
-
-        CREATE TABLE IF NOT EXISTS public.card_data(
-            id serial PRIMARY KEY,
-            account_id integer not null,
-            card_num VARCHAR (64) NOT NULL,
-            card_owner VARCHAR (255) NOT NULL,
-            card_exp TIMESTAMP NOT NULL,
-            card_cvv VARCHAR (64) NOT NULL,
+            data_type datatype not null,
+            data_info TEXT NOT NULL,
             meta VARCHAR (255),
             uploaded_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
             FOREIGN KEY (account_id) REFERENCES public.account (id) ON DELETE CASCADE
